2a8b8e9dfae30a6d4ea1414d6a5c244799fce15a
Juan Frauca <juanfrauca@outlook.com>
2025-01-28 17:05:28 +0100

    update

---
+++ TEMA 4 JAVASCRIPT/18-18-2024/BUCLES/ejercicio10.js
@@ -0,0 +1,8 @@
+/*
+Realiza un programa que muestre por pantalla 
+los números del 1 al 100 sin mostrar aquellos números múltiplos de 5.
+*/
+
+for (let i = 1 ; i<=100 ; i++){
+    if(i%5 == 0){console.log(i)}
+}
--- TEMA 4 JAVASCRIPT/18-18-2024/BUCLES/ejercicio9.js
+++ TEMA 4 JAVASCRIPT/18-18-2024/BUCLES/ejercicio9.js
@@ -3,3 +3,8 @@ Escribe un programa que muestre los números del 1 al 12 en una columna.
 En la columna de los números impares mostrar el cuadrado de ese valor y en la de los pares mostrar el cubo.
 */
 
+console.log("Pares || Cubos     Impares || Cubos");
+console.log("--------------------------------");
+for (let i = 1; i <= 12; i+=2) {
+    console.log(`${i+1}    || ${(i+1)*(i+1)*(i+1)}        ${i}     || ${i*i}`);
+}
