6986506c35008891ac89ad6c029acffd93ede1a0
Juan Frauca <juanfrauca@outlook.com>
2025-02-04 11:56:21 +0100

    Seguimos con Funciones

--- TEMA 4 JAVASCRIPT/15-01-2025/ejercicio2.js
+++ TEMA 4 JAVASCRIPT/15-01-2025/ejercicio2.js
@@ -2,3 +2,16 @@
 . Escribe una función que escriba “Hola” tantas veces como el número que le pasamos de parámetro. Llámala dos veces, primero
 con 5 y después con 3.
 */
+
+
+function saludar(numero){
+    for(i=0;i<numero;i++){
+        console.log("HOLA")
+    }
+}
+
+
+
+saludar(3)
+console.log("--------")
+saludar(5)
--- TEMA 4 JAVASCRIPT/15-01-2025/ejercicio3.js
+++ TEMA 4 JAVASCRIPT/15-01-2025/ejercicio3.js
@@ -1,3 +1,10 @@
 /*
 Escribe una función que devuelva el resultado de sumar los dos parámetros que le pasamos. Muestra el resultado en la consola.
 */
+
+function sumar (numero1,numero2){
+    const resultado=numero1+numero2
+    console.log(resultado)
+}
+
+sumar(5,4)
